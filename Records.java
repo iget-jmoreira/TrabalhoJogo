4e4ee8b1ba5ea7fed017bc5d588b39c9c3f6da8e
Airton Filho <jmoreira@iget.com.br>
2015-05-08 10:03:32 -0300

    implementando JTable

--- Records.java
+++ Records.java
@@ -1,7 +1,7 @@
 package TrabalhoJogo;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.GridBagLayout;
+//import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 
 
 public class Records extends JFrame{
@@ -24,44 +25,52 @@ public class Records extends JFrame{
 
 	public Records(String username){
 		setTitle("Records");
-		GridBagLayout layout = new GridBagLayout();
-		painel.setLayout(layout);
+//		GridBagLayout layout = new GridBagLayout();
+		painel.setLayout(null);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double width = (screenSize.getWidth() / 2) - 300;
 		double height = (screenSize.getHeight() / 2) - 300;
 		setBounds((int) width, (int) height, 600, 600);
 		
-		user = new JLabel("Username");
-		painel.add(user, new GBC(1,1,1,1).setWeight(1, 1).setAnchor(GBC.EAST).setInsets(0, 10, 0, 0));
-		score = new JLabel("Score");
-		painel.add(score, new GBC(2,1,1,1).setWeight(1, 1).setAnchor(GBC.CENTER).setInsets(0, 10, 0, 0));
-		data = new JLabel("Date");
-		painel.add(data, new GBC(3,1,1,1).setWeight(1, 1).setAnchor(GBC.WEST).setInsets(0, 10, 0, 0));
+		String[] columns = {"Username", "Score", "Date"};
+		Object[][] data = {
+				{"airton", "2342", "hoje"},
+				{"airton", "2342", "hoje"},
+				{"airton", "2342", "hoje"},
+				{"airton", "2342", "hoje"},
+				{"airton", "2342", "hoje"},
+				{"airton", "2342", "hoje"},
+				{"airton", "2342", "hoje"}
+		};
 		
-		Connect c = new Connect();
-		String users[] = c.getUsersRecord();
-		String datas[] = c.getDatasRecord();
-		Integer[] scores = c.getScoresRecord();
-		for(int i = 1; i<=10; i++){
-			if((users[i] != null)){
-				user = new JLabel(users[i]);
-				painel.add(user, new GBC(1,1+i,1,1).setWeight(1, 1).setAnchor(GBC.EAST).setInsets(0, 0, 0, 0));
-				score = new JLabel(scores[i] + "");
-				painel.add(score, new GBC(2,1+i,1,1).setWeight(1, 1).setAnchor(GBC.CENTER).setInsets(0, 0, 0, 0));
-				data = new JLabel(datas[1]);
-				painel.add(data, new GBC(3,1+i,1,1).setWeight(1, 1).setAnchor(GBC.WEST).setInsets(0, 0, 0, 0));
-			} else{
-				user = new JLabel("");
-				painel.add(user, new GBC(1,1+i,1,1).setWeight(1, 1).setAnchor(GBC.EAST).setInsets(0, 0, 0, 0));
-				score = new JLabel("");
-				painel.add(score, new GBC(2,1+i,1,1).setWeight(1, 1).setAnchor(GBC.CENTER).setInsets(0, 0, 0, 0));
-				data = new JLabel("");
-				painel.add(data, new GBC(3,1+i,1,1).setWeight(1, 1).setAnchor(GBC.WEST).setInsets(0, 20, 0, 20));
-			}
-		}
+		JTable table = new JTable(data, columns);
+		painel.add(table);
+//		Connect c = new Connect();
+//		String users[] = c.getUsersRecord();
+//		String datas[] = c.getDatasRecord();
+//		Integer[] scores = c.getScoresRecord();
+//		for(int i = 1; i<=10; i++){
+//			if((users[i] != null)){
+//				user = new JLabel(users[i]);
+//				painel.add(user, new GBC(1,1+i,1,1).setWeight(1, 1).setAnchor(GBC.EAST).setInsets(0, 0, 0, 0));
+//				score = new JLabel(scores[i] + "");
+//				painel.add(score, new GBC(2,1+i,1,1).setWeight(1, 1).setAnchor(GBC.CENTER).setInsets(0, 0, 0, 0));
+//				data = new JLabel(datas[1]);
+//				painel.add(data, new GBC(3,1+i,1,1).setWeight(1, 1).setAnchor(GBC.WEST).setInsets(0, 0, 0, 0));
+//			} else{
+//				user = new JLabel("");
+//				painel.add(user, new GBC(1,1+i,1,1).setWeight(1, 1).setAnchor(GBC.EAST).setInsets(0, 0, 0, 0));
+//				score = new JLabel("");
+//				painel.add(score, new GBC(2,1+i,1,1).setWeight(1, 1).setAnchor(GBC.CENTER).setInsets(0, 0, 0, 0));
+//				data = new JLabel("");
+//				painel.add(data, new GBC(3,1+i,1,1).setWeight(1, 1).setAnchor(GBC.WEST).setInsets(0, 20, 0, 20));
+//			}
+//		}
 		
 		submit_back = new JButton("Voltar");
-		painel.add(submit_back, new GBC(2,12,1,1).setWeight(1, 0.3).setAnchor(GBC.CENTER).setInsets(0, 10, 15, 10).setFill(GBC.BOTH));
+		submit_back.setBounds(250, 540, 100, 30);
+		painel.add(submit_back);
+//		painel.add(submit_back, new GBC(2,12,1,1).setWeight(1, 0.3).setAnchor(GBC.CENTER).setInsets(0, 10, 15, 10).setFill(GBC.BOTH));
 		
 		ClickBack back = new ClickBack(username);
 		submit_back.addActionListener(back);
